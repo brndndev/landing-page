0a454b242b4b7427aea11cca620cfea1bf712185
Brandon Price <brndndev@icloud.com>
2022-07-29 14:44:45 -0400

    added styling for active state, make code function across devices, make code consistent  and logical

--- js/app.js
+++ js/app.js
@@ -13,14 +13,17 @@
  * 
 */
 
-
+//make sure the DOM is properly loaded
 window.addEventListener('DOMContentLoaded', (event) => {
 
     /**
  * Define Global Variables
  * 
 */
+
+//gather all the sections
 let sections = document.querySelectorAll("section")
+//get the navbar list
 let navbarList = document.querySelector("#navbar__list")
 
 /**
@@ -32,56 +35,60 @@ let navbarList = document.querySelector("#navbar__list")
 */
 
     // build the nav
-
     function buildNav(){
         //loop through the number of sections, for each section  
         for (let i = 1; i <= sections.length; i++){
             //create an a tag and a li tag, append the li tag to a tag
             const a = document.createElement("a")
+            //give each anchor tag an id 
             a.setAttribute("id", "section" + i)
-
+            //add event listener to every anchor tag
             a.addEventListener("click", function (event){
+                //call the scrollToAnchor function
                 scrollToAnchor(event)
             })
-
+            //create li element 
             const li = document.createElement("li")
+            //add text content to the list element denoting the section
             li.textContent = "Section " + i
+            //append the li element to the anchor tag
             a.appendChild(li)
             //then append the a tag to the navbar list
             navbarList.appendChild(a)
         }
     }
 
-    // Add class 'active' to section when near top of viewport
+        // Add class 'active' to section when near top of viewport
+    function addActiveClass(){
+        window.addEventListener('scroll', (event) => {
 
-    window.addEventListener('scroll', (event) => {
-
-        //gather each section
-        const sections = document.querySelectorAll('section');
-      
-        // loop through each section
-        for (section of sections){
-            //get top distance
-            const top = section.getBoundingClientRect().top
-            //check if top is in viewport, if so add class, if not remove the class
-            if (top > 0 && top < 100){
-                section.style.backgroundColor = "#999999"
-                section.classList.add("active")
-            }else{
-                section.style.backgroundColor = ""
-                section.classList.remove("active")
+            //gather each section
+            const sections = document.querySelectorAll('section');
+            
+            // loop through each section
+            for (section of sections){
+                //get top distance
+                const top = section.getBoundingClientRect().top
+                //check if top is in viewport, if so add class, if not remove the class
+                if (top > 0 && top < 100){
+                    //add active class
+                    section.classList.add("active")
+                }else{
+                    //remove active class
+                    section.classList.remove("active")
+                }
             }
-        }
-      });
+        });
+    }
 
     // Scroll to anchor ID using scrollTO event
-
     function scrollToAnchor(event){
         //get the id of the slected anchor tag 
         const elemID = event.target.parentElement.id
 
         //find the section needed to scroll to and add scroll into view functionality to it with a behavior of smooth
         document.querySelectorAll("section#" + elemID)[0].scrollIntoView({
+            //add smooth scrolling
             behavior: 'smooth'
           })
 
@@ -96,7 +103,8 @@ let navbarList = document.querySelector("#navbar__list")
 //build menu
 buildNav()
 
-
+//add active class
+addActiveClass()
 
 });
 
